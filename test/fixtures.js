004b746b4deabd3e81bc23ff0de543c390fab01b
Ivan Shumkov <ivan@shumkov.ru>
2020-12-01 21:37:06 +0800

    tests: update server tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,14 +1,21 @@
 const { varint } = require('protocol-buffers-encodings')
 
-const { tendermint: { abci: { Request, RequestInfo, RequestFlush } } } = require('../types');
+const {
+  tendermint: {
+    abci: {
+      Request,
+      Response,
+    }
+  }
+} = require('../types');
 
-function encodeRequest(message) {
-  let err = Request.verify(infoRequest);
+function encodeDelimited(MessageClass, message) {
+  let err = MessageClass.verify(infoRequest);
   if (err) {
     throw new Error(err);
   }
 
-  const messageBytes = Request.encode(message).finish();
+  const messageBytes = MessageClass.encode(message).finish();
 
   let lengthBytes = Buffer.from(
     varint.encode(messageBytes.length << 1),
@@ -18,36 +25,54 @@ function encodeRequest(message) {
 }
 
 const infoRequest = new Request({
-  info: new RequestInfo({
+  info: {
     version: '0.19.2-64408a40'
-  }),
+  },
 });
 
-const infoRequestBytes = encodeRequest(infoRequest);
+const infoRequestBytes = encodeDelimited(Request, infoRequest);
+
+const infoResponse = new Response({
+  info: {
+    data: 'test',
+    version: '123',
+    lastBlockHeight: 5,
+    lastBlockAppHash: Buffer.alloc(0),
+  }
+});
+
+const infoResponseBytes = encodeDelimited(Response, infoResponse);
+
+const echoMessage = {
+  echo: {
+    message: 'hello!',
+  },
+};
+
+const echoRequest = new Request(echoMessage);
+
+const echoRequestBytes = encodeDelimited(Request, echoRequest);
+
+const echoResponse = new Response(echoMessage);
+
+const echoResponseBytes = encodeDelimited(Response, echoResponse);
 
 const flushRequest = new Request({
-  flush: new RequestFlush(),
+  flush: { },
 });
 
-const flushRequestBytes = encodeRequest(flushRequest);
+const flushRequestBytes = encodeDelimited(Request, flushRequest);
 
 module.exports = {
   multiRequestBytes: Buffer.concat([infoRequestBytes, flushRequestBytes]),
   infoRequestBytes: infoRequestBytes,
   flushRequestBytes: flushRequestBytes,
-  echoRequestBytes: Buffer.from('0e12050a03010203', 'hex'),
+  echoRequestBytes: echoRequestBytes,
   infoRequest: infoRequest.toJSON(),
-  infoResponse: {
-    info: {
-      data: 'test',
-      version: '123',
-      lastBlockHeight: 5,
-      lastBlockAppHash: Buffer.alloc(0)
-    }
-  },
-  infoResponseHex: '22220f0a0474657374120331323320052a00',
+  infoResponse: infoResponse.toJSON(),
+  infoResponseHex: infoResponseBytes.toString('hex'),
   emptyInfoResponseHex: '042200',
   flushResponseHex: '041a00',
-  echoResponseHex: '0e12050a03010203',
+  echoResponseHex: echoResponseBytes.toString('hex'),
   exceptionResponseHex: '2a0a130a114572726f723a2074657374206572726f72'
 }
