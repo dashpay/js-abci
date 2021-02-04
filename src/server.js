fca3c07873eb5a09c4cc7359c4f48df53ac23030
Ivan Shumkov <ivan@shumkov.ru>
2021-02-04 22:54:01 +0800

    style: fix linter

--- src/server.js
+++ src/server.js
@@ -56,11 +56,11 @@ function createServer (app) {
       } catch (err) {
         fail(err)
       }
-    });
+    })
 
     conn.on('error', (error) => {
-      server.emit('error', error);
-    });
+      server.emit('error', error)
+    })
   })
 
   return server
