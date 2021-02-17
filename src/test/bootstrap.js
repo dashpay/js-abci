176032998c242ea742d17d447a6e777ff0d3fef6
Djavid Gabibiyan <djavidoff@gmail.com>
2021-02-17 15:18:41 +0500

    chore: setup sinon

--- src/test/bootstrap.js
+++ src/test/bootstrap.js
@@ -1,7 +1,22 @@
 const { expect, use } = require('chai');
+const sinon = require('sinon');
 const dirtyChai = require('dirty-chai');
+const sinonChai = require('sinon-chai');
 
 use(dirtyChai);
+use(sinonChai);
+
+beforeEach(function beforeEach() {
+  if (!this.sinonSandbox) {
+    this.sinonSandbox = sinon.createSandbox();
+  } else {
+    this.sinonSandbox.restore();
+  }
+});
+
+afterEach(function afterEach() {
+  this.sinonSandbox.restore();
+});
 
 process.env.NODE_ENV = 'test';
 
